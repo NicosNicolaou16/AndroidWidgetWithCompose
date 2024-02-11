690051f8e189291e061412678f5b4a4da5517105
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-02-11 17:52:05 +0200

    add permissions

--- app/src/main/java/com/nicos/androidwidgetwithcompose/MainActivity.kt
+++ app/src/main/java/com/nicos/androidwidgetwithcompose/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.nicos.androidwidgetwithcompose
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
 import com.nicos.androidwidgetwithcompose.ui.theme.AndroidWidgetWithComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,9 +28,48 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    val permissionLauncher = rememberLauncherForActivityResult(
+                        ActivityResultContracts.RequestMultiplePermissions()
+                    ) { _ -> }
+                    Box(contentAlignment = Alignment.Center,
+                        modifier = Modifier.clickable {
+                            requestForPermissionAndStartTheService(permissionLauncher)
+                        }) {
+                        Text(
+                            text = "Widget Test\nClick Here for location permission",
+                            textAlign = TextAlign.Center,
+                        )
+                    }
                 }
             }
         }
+    }
+}
+
+private fun requestForPermissionAndStartTheService(permissionLauncher: ManagedActivityResultLauncher<Array<String>, Map<String, @JvmSuppressWildcards Boolean>>) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        permissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+                Manifest.permission.POST_NOTIFICATIONS
+            )
+        )
+    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        permissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION,
+            )
+        )
+    } else {
+        permissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            )
+        )
     }
 }
