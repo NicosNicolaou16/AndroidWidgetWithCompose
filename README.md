4a48206ca81f14642ba7f9559223bd53ea85c3b1
Nicos Nicolaou <nicosnicos16@gmail.com>
2024-05-05 23:09:01 +0300

    update the Gradle to latest version, update some dependencies libraries and update README file

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ https://github.com/NicosNicolaou16/Android_Widget_With_Compose_With_Flutter <br 
 Target SDK version: 34 <br />
 Minimum SDK version: 27 <br />
 Kotlin version: 1.9.23 <br />
-Gradle version: 8.3.2 <br />
+Gradle version: 8.4.0 <br />
 
 ## Step 1 add the follow dependencies libraries
 
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         buildConfig = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.11"
+        kotlinCompilerExtensionVersion = "1.5.13"
     }
     packaging {
         resources {
@@ -55,10 +55,10 @@ val composeGlanceWidgetVersion by extra("1.0.0")
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
@@ -66,7 +66,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2024.04.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.05.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
