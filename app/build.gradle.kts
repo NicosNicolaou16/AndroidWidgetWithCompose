b3f0d59d77db381e715f36001c9179689c2469f2
Nicos Nicolaou <nicosnicos16@gmail.com>
2026-03-07 00:10:15 +0200

    update Activity Compose version to 2.10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,7 +61,7 @@ val composeGlanceWidgetVersion by extra("1.1.1")
 dependencies {
 
     implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
     implementation("androidx.activity:activity-compose:1.11.0")
     implementation(platform("androidx.compose:compose-bom:2025.09.00"))
     implementation("androidx.compose.ui:ui")
